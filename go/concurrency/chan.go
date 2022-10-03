dfea9f78a7cfb5eb0d2bdb042af3c68c384ff4c8
CShaZi <primaryemail@qq.com>
2022-10-03 17:09:08 +0800

    补充go

--- go/concurrency/chan.go
+++ go/concurrency/chan.go
@@ -8,19 +8,20 @@ import (
 
 const LoopSize int = 100
 
-func PrintTime(normal, done *chan int) {
+func PrintTime(normal, done chan int) {
+	fmt.Println(&normal, &done)
 	isDone := false
 	for !isDone {
 		select {
-		case <-*normal:
-			fmt.Println(time.Now().Format("2006-01-02 15:04:05.000000"))
-		case <-*done:
+		case v := <-normal:
+			fmt.Printf("[%s] %v\n", time.Now().Format("2006-01-02 15:04:05.000000"), v)
+		case <-done:
 			fmt.Println("done")
 			isDone = true
 		default:
-			fmt.Println("default")
+			fmt.Println("empty")
+			time.Sleep(time.Millisecond * 500)
 		}
-		time.Sleep(time.Millisecond * 500)
 	}
 }
 
@@ -28,17 +29,20 @@ func main() {
 	normal := make(chan int, 1)
 	done := make(chan int, 1)
 
+	fmt.Println(&normal, &done)
+
 	go func() {
 		for {
 			num := rand.Intn(100)
 			if num == 0 {
 				done <- num
-				time.Sleep(time.Millisecond * 100)
+				break
 			} else {
 				normal <- num
-				time.Sleep(time.Millisecond * 1000)
+				time.Sleep(time.Millisecond * 100)
 			}
 		}
 	}()
-	PrintTime(&normal, &done)
+
+	PrintTime(normal, done)
 }
