e2296a5f71075dbf61e6990f6806f6557f4e392d
primaryshazi <primaryemail@qq.com>
2022-12-15 11:53:39 +0800

    修改a start

--- algorithm/a start/AStar.cpp
+++ algorithm/a start/AStar.cpp
@@ -6,171 +6,171 @@ using namespace std::placeholders;
 
 AStar::PathNode::PathNode(Vec2i coordinates_, PathNode *parent_)
 {
-	parent = parent_;
-	coordinates = coordinates_;
-	G = H = 0;
+    parent = parent_;
+    coordinates = coordinates_;
+    G = H = 0;
 }
 
 AStar::uint AStar::PathNode::getScore()
 {
-	return G + H;
+    return G + H;
 }
 
 AStar::Generator::Generator()
 {
-	setDiagonalMovement(false);
-	setHeuristic(&Heuristic::manhattan);
-	direction = {
-		{ 0, 1 },{ 1, 0 },{ 0, -1 },{ -1, 0 },
-		{ -1, -1 },{ 1, 1 },{ -1, 1 },{ 1, -1 }
-	};
+    setDiagonalMovement(false);
+    setHeuristic(&Heuristic::manhattan);
+    direction = {
+        { 0, 1 },{ 1, 0 },{ 0, -1 },{ -1, 0 },
+        { -1, -1 },{ 1, 1 },{ -1, 1 },{ 1, -1 }
+    };
 }
 
 void AStar::Generator::setWorldSize(Vec2i worldSize_)
 {
-	worldSize = worldSize_;
+    worldSize = worldSize_;
 }
 
 void AStar::Generator::setDiagonalMovement(bool enable_)
 {
-	directions = (enable_ ? 8 : 4);
+    directions = (enable_ ? 8 : 4);
 }
 
 void AStar::Generator::setHeuristic(HeuristicFunction heuristic_)
 {
-	heuristic = std::bind(heuristic_, _1, _2);
+    heuristic = std::bind(heuristic_, _1, _2);
 }
 
 void AStar::Generator::addCollision(Vec2i coordinates_)
 {
-	walls.emplace(coordinates_);
+    walls.emplace(coordinates_);
 }
 
 void AStar::Generator::removeCollision(Vec2i coordinates_)
 {
-	walls.erase(coordinates_);
+    walls.erase(coordinates_);
 }
 
 void AStar::Generator::clearCollisions()
 {
-	walls.clear();
+    walls.clear();
 }
 
 void AStar::Generator::clear()
 {
-	heuristic = nullptr;
-	worldSize = Vec2i(0, 0);
-	walls.clear();
+    heuristic = nullptr;
+    worldSize = Vec2i(0, 0);
+    walls.clear();
 }
 
 AStar::CoordinateList AStar::Generator::findPath(Vec2i source_, Vec2i target_)
 {
-	PathNode *current = nullptr;
-	NodeSet openSet, closedSet;
-	openSet.emplace(new PathNode(source_));
+    PathNode *current = nullptr;
+    NodeSet openSet, closedSet;
+    openSet.emplace(new PathNode(source_));
 
-	while (!openSet.empty()) {
-		auto current_it = openSet.begin();
-		current = *current_it;
+    while (!openSet.empty()) {
+        auto current_it = openSet.begin();
+        current = *current_it;
 
-		for (auto it = openSet.begin(); it != openSet.end(); it++) {
-			auto node = *it;
-			if (node->getScore() <= current->getScore()) {
-				current = node;
-				current_it = it;
-			}
-		}
+        for (auto it = openSet.begin(); it != openSet.end(); it++) {
+            auto node = *it;
+            if (node->getScore() <= current->getScore()) {
+                current = node;
+                current_it = it;
+            }
+        }
 
-		if (current->coordinates == target_) {
-			break;
-		}
+        if (current->coordinates == target_) {
+            break;
+        }
 
-		closedSet.emplace(current);
-		openSet.erase(current_it);
+        closedSet.emplace(current);
+        openSet.erase(current_it);
 
-		for (uint i = 0; i < directions; ++i) {
-			Vec2i newCoordinates(current->coordinates + direction[i]);
-			if (detectCollision(newCoordinates) ||
-				findNodeOnList(closedSet, newCoordinates)) {
-				continue;
-			}
+        for (uint i = 0; i < directions; ++i) {
+            Vec2i newCoordinates(current->coordinates + direction[i]);
+            if (detectCollision(newCoordinates) ||
+                findNodeOnList(closedSet, newCoordinates)) {
+                continue;
+            }
 
-			uint totalCost = current->G + ((i < 4) ? 10 : 14);
+            uint totalCost = current->G + ((i < 4) ? 10 : 14);
 
-			PathNode *successor = findNodeOnList(openSet, newCoordinates);
-			if (successor == nullptr) {
-				successor = new PathNode(newCoordinates, current);
-				successor->G = totalCost;
-				successor->H = heuristic(successor->coordinates, target_);
-				openSet.emplace(successor);
-			}
-			else if (totalCost < successor->G) {
-				successor->parent = current;
-				successor->G = totalCost;
-			}
-		}
-	}
+            PathNode *successor = findNodeOnList(openSet, newCoordinates);
+            if (successor == nullptr) {
+                successor = new PathNode(newCoordinates, current);
+                successor->G = totalCost;
+                successor->H = heuristic(successor->coordinates, target_);
+                openSet.emplace(successor);
+            }
+            else if (totalCost < successor->G) {
+                successor->parent = current;
+                successor->G = totalCost;
+            }
+        }
+    }
 
-	CoordinateList path;
-	while (current != nullptr) {
-		path.push_back(current->coordinates);
-		current = current->parent;
-	}
+    CoordinateList path;
+    while (current != nullptr) {
+        path.push_back(current->coordinates);
+        current = current->parent;
+    }
 
-	releaseNodes(openSet);
-	releaseNodes(closedSet);
+    releaseNodes(openSet);
+    releaseNodes(closedSet);
 
-	return path;
+    return path;
 }
 
 AStar::PathNode *AStar::Generator::findNodeOnList(NodeSet &nodes_, Vec2i coordinates_)
 {
-	auto n = PathNode(coordinates_);
-	auto it = nodes_.find(&n);
-	if (it != nodes_.end())
-	{
-		return *it;
-	}
-	return nullptr;
+    auto n = PathNode(coordinates_);
+    auto it = nodes_.find(&n);
+    if (it != nodes_.end())
+    {
+        return *it;
+    }
+    return nullptr;
 }
 
 void AStar::Generator::releaseNodes(NodeSet &nodes_)
 {
-	for (auto it = nodes_.begin(); it != nodes_.end();) {
-		delete *it;
-		it = nodes_.erase(it);
-	}
+    for (auto it = nodes_.begin(); it != nodes_.end();) {
+        delete *it;
+        it = nodes_.erase(it);
+    }
 }
 
 bool AStar::Generator::detectCollision(Vec2i coordinates_)
 {
-	if (coordinates_.x < 0 || coordinates_.x >= worldSize.x ||
-		coordinates_.y < 0 || coordinates_.y >= worldSize.y ||
-		walls.find(coordinates_) != walls.end()) {
-		return true;
-	}
-	return false;
+    if (coordinates_.x < 0 || coordinates_.x >= worldSize.x ||
+        coordinates_.y < 0 || coordinates_.y >= worldSize.y ||
+        walls.find(coordinates_) != walls.end()) {
+        return true;
+    }
+    return false;
 }
 
 AStar::Vec2i AStar::Heuristic::getDelta(Vec2i source_, Vec2i target_)
 {
-	return{ abs(source_.x - target_.x),  abs(source_.y - target_.y) };
+    return{ abs(source_.x - target_.x),  abs(source_.y - target_.y) };
 }
 
 AStar::uint AStar::Heuristic::manhattan(Vec2i source_, Vec2i target_)
 {
-	auto delta = std::move(getDelta(source_, target_));
-	return static_cast<uint>(10 * (delta.x + delta.y));
+    auto delta = std::move(getDelta(source_, target_));
+    return static_cast<uint>(10 * (delta.x + delta.y));
 }
 
 AStar::uint AStar::Heuristic::euclidean(Vec2i source_, Vec2i target_)
 {
-	auto delta = std::move(getDelta(source_, target_));
-	return static_cast<uint>(10 * sqrt(pow(delta.x, 2) + pow(delta.y, 2)));
+    auto delta = std::move(getDelta(source_, target_));
+    return static_cast<uint>(10 * sqrt(pow(delta.x, 2) + pow(delta.y, 2)));
 }
 
 AStar::uint AStar::Heuristic::octagonal(Vec2i source_, Vec2i target_)
 {
-	auto delta = std::move(getDelta(source_, target_));
-	return 10 * (delta.x + delta.y) + (-6) * std::min(delta.x, delta.y);
+    auto delta = std::move(getDelta(source_, target_));
+    return 10 * (delta.x + delta.y) + (-6) * std::min(delta.x, delta.y);
 }
--- algorithm/a start/AStar.h
+++ algorithm/a start/AStar.h
@@ -12,164 +12,164 @@ For more information visit https://opensource.org/licenses/ISC.
 
 namespace AStar
 {
-	struct Vec2i
-	{
-		int x = 0;
-		int y = 0;
+struct Vec2i
+{
+    int x = 0;
+    int y = 0;
 
-		Vec2i() {}
-		Vec2i(int x_, int y_) : x(x_), y(y_) {}
-		Vec2i(const Vec2i &v) : x(v.x), y(v.y) {}
-		Vec2i &operator=(const Vec2i &v) {
-			x = v.x; y = v.y; return *this;
-		}
-		~Vec2i() {}
+    Vec2i() {}
+    Vec2i(int x_, int y_): x(x_), y(y_) {}
+    Vec2i(const Vec2i &v): x(v.x), y(v.y) {}
+    Vec2i &operator=(const Vec2i &v) {
+        x = v.x; y = v.y; return *this;
+    }
+    ~Vec2i() {}
 
-		friend bool operator==(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			return (a.x == b.x && a.y == b.y);
-		}
+    friend bool operator==(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        return (a.x == b.x && a.y == b.y);
+    }
 
-		friend bool operator!=(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			return (a.x != b.x || a.y != b.y);
-		}
+    friend bool operator!=(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        return (a.x != b.x || a.y != b.y);
+    }
 
-		friend bool operator >(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			if (a.x > b.x) {
-				return true;
-			}
-			if (a.x < b.x) {
-				return false;
-			}
-			return a.y > b.y;
-		}
+    friend bool operator >(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        if (a.x > b.x) {
+            return true;
+        }
+        if (a.x < b.x) {
+            return false;
+        }
+        return a.y > b.y;
+    }
 
-		friend bool operator <(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			if (a.x < b.x) {
-				return true;
-			}
-			if (a.x > b.x) {
-				return false;
-			}
-			return a.y < b.y;
-		}
+    friend bool operator <(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        if (a.x < b.x) {
+            return true;
+        }
+        if (a.x > b.x) {
+            return false;
+        }
+        return a.y < b.y;
+    }
 
-		friend bool operator>=(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			return !(a < b);
-		}
+    friend bool operator>=(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        return !(a < b);
+    }
 
-		friend bool operator<=(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			return !(a > b);
-		}
+    friend bool operator<=(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        return !(a > b);
+    }
 
-		friend AStar::Vec2i operator +(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			return{ a.x + b.x, a.y + b.y };
-		}
+    friend AStar::Vec2i operator +(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        return{ a.x + b.x, a.y + b.y };
+    }
 
-		friend AStar::Vec2i operator -(const AStar::Vec2i &a, const AStar::Vec2i &b) {
-			return{ a.x - b.x, a.y - b.y };
-		}
-	};
+    friend AStar::Vec2i operator -(const AStar::Vec2i &a, const AStar::Vec2i &b) {
+        return{ a.x - b.x, a.y - b.y };
+    }
+};
 
-	using uint = unsigned int;
-	using HeuristicFunction = std::function<uint(Vec2i, Vec2i)>;
-	using CoordinateList = std::vector<Vec2i>;
-	using CoordinateSet = std::set<Vec2i>;
+using uint = unsigned int;
+using HeuristicFunction = std::function<uint(Vec2i, Vec2i)>;
+using CoordinateList = std::vector<Vec2i>;
+using CoordinateSet = std::set<Vec2i>;
 
-	struct PathNode
-	{
-		uint G = 0;
-		uint H = 0;
-		Vec2i coordinates;
-		PathNode *parent = nullptr;
+struct PathNode
+{
+    uint G = 0;
+    uint H = 0;
+    Vec2i coordinates;
+    PathNode *parent = nullptr;
 
-		PathNode(Vec2i coord_, PathNode *parent_ = nullptr);
-		uint getScore();
+    PathNode(Vec2i coord_, PathNode *parent_ = nullptr);
+    uint getScore();
 
-		friend bool operator==(const AStar::PathNode &a, const AStar::PathNode &b) {
-			return a.coordinates == b.coordinates;
-		}
+    friend bool operator==(const AStar::PathNode &a, const AStar::PathNode &b) {
+        return a.coordinates == b.coordinates;
+    }
 
-		friend bool operator!=(const AStar::PathNode &a, const AStar::PathNode &b) {
-			return a.coordinates != b.coordinates;
-		}
+    friend bool operator!=(const AStar::PathNode &a, const AStar::PathNode &b) {
+        return a.coordinates != b.coordinates;
+    }
 
-		friend bool operator >(const AStar::PathNode &a, const AStar::PathNode &b) {
-			return a.coordinates > b.coordinates;
-		}
+    friend bool operator >(const AStar::PathNode &a, const AStar::PathNode &b) {
+        return a.coordinates > b.coordinates;
+    }
 
-		friend bool operator <(const AStar::PathNode &a, const AStar::PathNode &b) {
-			return a.coordinates < b.coordinates;
-		}
+    friend bool operator <(const AStar::PathNode &a, const AStar::PathNode &b) {
+        return a.coordinates < b.coordinates;
+    }
 
-		friend bool operator<=(const AStar::PathNode &a, const AStar::PathNode &b) {
-			return a.coordinates <= b.coordinates;
-		}
+    friend bool operator<=(const AStar::PathNode &a, const AStar::PathNode &b) {
+        return a.coordinates <= b.coordinates;
+    }
 
-		friend bool operator>=(const AStar::PathNode &a, const AStar::PathNode &b) {
-			return a.coordinates >= b.coordinates;
-		}
+    friend bool operator>=(const AStar::PathNode &a, const AStar::PathNode &b) {
+        return a.coordinates >= b.coordinates;
+    }
 
-		struct PathNodeComp
-		{
-			bool operator() (const PathNode *na, const PathNode *nb) const
-			{
-				if (nullptr == na && nb == nullptr)
-				{
-					return false;
-				}
+    struct PathNodeComp
+    {
+        bool operator() (const PathNode *na, const PathNode *nb) const
+        {
+            if (nullptr == na && nullptr == nb)
+            {
+                return false;
+            }
 
-				if (nullptr == na)
-				{
-					return true;
-				}
+            if (nullptr == na)
+            {
+                return true;
+            }
 
-				if (nullptr == nb)
-				{
-					return false;
-				}
+            if (nullptr == nb)
+            {
+                return false;
+            }
 
-				return *na < *nb;
-			}
-		};
-	};
+            return *na < *nb;
+        }
+    };
+};
 
-	using NodeSet = std::set<PathNode *, PathNode::PathNodeComp>;
+using NodeSet = std::set<PathNode *, PathNode::PathNodeComp>;
 
 
-	class Generator
-	{
-		bool detectCollision(Vec2i coordinates_);
-		PathNode *findNodeOnList(NodeSet &nodes_, Vec2i coordinates_);
-		void releaseNodes(NodeSet &nodes_);
+class Generator
+{
+    bool detectCollision(Vec2i coordinates_);
+    PathNode *findNodeOnList(NodeSet &nodes_, Vec2i coordinates_);
+    void releaseNodes(NodeSet &nodes_);
 
-	public:
-		Generator();
-		void setWorldSize(Vec2i worldSize_);
-		void setDiagonalMovement(bool enable_);
-		void setHeuristic(HeuristicFunction heuristic_);
-		CoordinateList findPath(Vec2i source_, Vec2i target_);
-		void addCollision(Vec2i coordinates_);
-		void removeCollision(Vec2i coordinates_);
-		void clearCollisions();
-		void clear();
+public:
+    Generator();
+    void setWorldSize(Vec2i worldSize_);
+    void setDiagonalMovement(bool enable_);
+    void setHeuristic(HeuristicFunction heuristic_);
+    CoordinateList findPath(Vec2i source_, Vec2i target_);
+    void addCollision(Vec2i coordinates_);
+    void removeCollision(Vec2i coordinates_);
+    void clearCollisions();
+    void clear();
 
-	private:
-		HeuristicFunction heuristic;
-		CoordinateList direction;
-		CoordinateSet walls;
-		Vec2i worldSize;
-		uint directions;
-	};
+private:
+    HeuristicFunction heuristic;
+    CoordinateList direction;
+    CoordinateSet walls;
+    Vec2i worldSize;
+    uint directions;
+};
 
-	class Heuristic
-	{
-		static Vec2i getDelta(Vec2i source_, Vec2i target_);
+class Heuristic
+{
+    static Vec2i getDelta(Vec2i source_, Vec2i target_);
 
-	public:
-		static uint manhattan(Vec2i source_, Vec2i target_);
-		static uint euclidean(Vec2i source_, Vec2i target_);
-		static uint octagonal(Vec2i source_, Vec2i target_);
-	};
+public:
+    static uint manhattan(Vec2i source_, Vec2i target_);
+    static uint euclidean(Vec2i source_, Vec2i target_);
+    static uint octagonal(Vec2i source_, Vec2i target_);
+};
 }
 
 #endif // __ASTAR_HPP_8F637DB91972F6C878D41D63F7E7214F__
